61579a151f43933fb8e3a0af57fc45e78d25ff0a
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-02-02 02:24:14 +0900

    flutter_secure_storage 패키지 사용. 로그인을 다시 할 시 토큰을 가지고 있으면 자동 로그인하는 기능. 안전하게.

--- actual/lib/user/view/login_screen.dart
+++ actual/lib/user/view/login_screen.dart
@@ -3,15 +3,27 @@ import 'dart:io';
 
 import 'package:actual/common/component/custom_text_form_field.dart';
 import 'package:actual/common/const/colors.dart';
+import 'package:actual/common/const/data.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/view/root_tab.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
+
+  @override
   Widget build(BuildContext context) {
+
     final dio = Dio();
 
     // localhost
@@ -39,7 +51,9 @@ class LoginScreen extends StatelessWidget {
                 ),
                 CustomTextFormField(
                   hintText: "이메일을 입력해주세요",
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    username = value;
+                  },
                 ),
                 const SizedBox(height: 16.0),
                 SizedBox(
@@ -48,27 +62,39 @@ class LoginScreen extends StatelessWidget {
                 CustomTextFormField(
                   hintText: "비밀번호를 입력해주세요",
                   obscureText: true,
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    password = value;
+                  },
                 ),
                 const SizedBox(height: 8.0),
                 ElevatedButton(
                     onPressed: () async {
                       // ID : 비밀번호
-                      final rawString = 'test@codefactoty.ai:testtest';
+                      final rawString = '$username:$password';
 
                       // Base64 로 인코딩하기
                       Codec<String, String> stringToBase64 = utf8.fuse(base64);
 
                       String token = stringToBase64.encode(rawString);
 
-                      final resp = await dio.post('http://$ip/auth/login',
+                      final resp = await dio.post(
+                        'http://$ip/auth/login',
                         options: Options(
                           headers: {
                             'Authorization': 'Basic $token',
                           },
                         ),
                       );
-                      print(resp.data);
+
+                      final refreshToken = resp.data['refreshToken'];
+                      final accessToken = resp.data['accessToken'];
+
+                      await storage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+                      await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (_) => RootTab(),
+                      ));
                     },
                     style: ElevatedButton.styleFrom(
                       primary: PRIMARY_COLOR,
@@ -84,7 +110,6 @@ class LoginScreen extends StatelessWidget {
                         },
                       ),
                     );
-                    print(resp.data);
                   },
                   style: TextButton.styleFrom(
                     primary: Colors.black,
